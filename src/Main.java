012af59db9da242535416ce6c24590f80df16597
Teekay <gustavoduarte.21233@gmail.com>
2024-07-16 19:39:40 -0300

    chess layer and printing board

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import boardgame.Position;
-
-public class Main {
-    public static void main(String[] args) {
-        Position pos = new Position(3,5);
-
-        System.out.println(pos   );
-    }
-}
---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+import chess.ChessMatch;
+
+public class Main {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+}
