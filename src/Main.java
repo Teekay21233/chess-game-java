efa5ea3ca7290c74e48525dee13743626c468238
Teekay <gustavoduarte.21233@gmail.com>
2024-07-16 18:53:56 -0300

    First class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position pos = new Position(3,5);
+
+        System.out.println(pos  );
     }
 }
