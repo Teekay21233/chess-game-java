1cc743c2097be69bfba3be4da7fccc893dcced37
Teekay <gustavoduarte.21233@gmail.com>
2024-07-16 20:59:20 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1){
+            throw new BoardException("Erro ao criar tabuleiro: é necessário haver pelo menos uma linha e uma coluna.");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,28 +18,44 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column){
+        if (!positionExists(row,column)){
+            throw new BoardException("Posição inexistente");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Posição inexistente");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if (thereIsAPiece(position)){
+            throw new BoardException("Ja existe uma peça na posição " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row,int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Posição inexistente");
+        }
+        return piece(position) != null;
     }
 }
